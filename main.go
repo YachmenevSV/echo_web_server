f92a12bd3dd8e1be5b5b32c20cf8fc09a3584cce
yachmenev sergei <yachmenevsv@gmail.com>
2024-09-05 15:25:38 +0300

    add custom response code

--- main.go
+++ main.go
@@ -8,10 +8,29 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"os"
+	"strconv"
 )
 
 func getRoot(w http.ResponseWriter, req *http.Request) {
 	fmt.Printf("got / request\n")
+
+	// Печать сообщения и ожидание ввода от пользователя
+	fmt.Printf("Enter the response status code or press Enter to use 200: ")
+	var userInput string
+	fmt.Scanln(&userInput)
+
+	// Если пользователь ввел код ответа, преобразуем его в int
+	statusCode := 200 // По умолчанию 200
+	if userInput != "" {
+		if code, err := strconv.Atoi(userInput); err == nil {
+			statusCode = code
+		} else {
+			fmt.Printf("Invalid input, using default 200\n")
+		}
+	}
+
+	w.WriteHeader(statusCode)
+	fmt.Printf("Set response code: %d\n", statusCode)
 	b, _ := httputil.DumpRequest(req, false)
 	io.WriteString(w, string(b))
 
@@ -20,13 +39,15 @@ func getRoot(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Printf("Wait while you press enter button\n")
-	fmt.Scanln()
+	// Устанавливаем код ответа
+
 	io.WriteString(w, "\nYour body:\n")
 	io.WriteString(w, string(b))
 }
 
 func main() {
+	fmt.Printf("Starting listen on localhost:41000 ")
+
 	http.HandleFunc("/", getRoot)
 	err := http.ListenAndServe(":41000", nil)
 
